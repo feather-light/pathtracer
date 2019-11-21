ac6acf9d7ae5b83a292ecd285430c2fde50ffa5b
feather-light <58053645+feather-light@users.noreply.github.com>
2019-11-22 01:23:36 +0200

    Initial upload

---
+++ build/CMakeFiles/pathtracer.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/konstantin/mycode/pathtracer/main.cpp" "/home/konstantin/mycode/pathtracer/build/CMakeFiles/pathtracer.dir/main.cpp.o"
+  "/home/konstantin/mycode/pathtracer/src/core/geometry/point.cpp" "/home/konstantin/mycode/pathtracer/build/CMakeFiles/pathtracer.dir/src/core/geometry/point.cpp.o"
+  "/home/konstantin/mycode/pathtracer/src/core/geometry/ray.cpp" "/home/konstantin/mycode/pathtracer/build/CMakeFiles/pathtracer.dir/src/core/geometry/ray.cpp.o"
+  "/home/konstantin/mycode/pathtracer/src/core/geometry/tuple.cpp" "/home/konstantin/mycode/pathtracer/build/CMakeFiles/pathtracer.dir/src/core/geometry/tuple.cpp.o"
+  "/home/konstantin/mycode/pathtracer/src/core/geometry/vector.cpp" "/home/konstantin/mycode/pathtracer/build/CMakeFiles/pathtracer.dir/src/core/geometry/vector.cpp.o"
+  "/home/konstantin/mycode/pathtracer/src/core/optics/emitter.cpp" "/home/konstantin/mycode/pathtracer/build/CMakeFiles/pathtracer.dir/src/core/optics/emitter.cpp.o"
+  "/home/konstantin/mycode/pathtracer/src/core/optics/laws.cpp" "/home/konstantin/mycode/pathtracer/build/CMakeFiles/pathtracer.dir/src/core/optics/laws.cpp.o"
+  "/home/konstantin/mycode/pathtracer/src/core/optics/material.cpp" "/home/konstantin/mycode/pathtracer/build/CMakeFiles/pathtracer.dir/src/core/optics/material.cpp.o"
+  "/home/konstantin/mycode/pathtracer/src/core/optics/redistributor.cpp" "/home/konstantin/mycode/pathtracer/build/CMakeFiles/pathtracer.dir/src/core/optics/redistributor.cpp.o"
+  "/home/konstantin/mycode/pathtracer/src/core/optics/reflector.cpp" "/home/konstantin/mycode/pathtracer/build/CMakeFiles/pathtracer.dir/src/core/optics/reflector.cpp.o"
+  "/home/konstantin/mycode/pathtracer/src/core/optics/spectrum.cpp" "/home/konstantin/mycode/pathtracer/build/CMakeFiles/pathtracer.dir/src/core/optics/spectrum.cpp.o"
+  "/home/konstantin/mycode/pathtracer/src/core/optics/transmitter.cpp" "/home/konstantin/mycode/pathtracer/build/CMakeFiles/pathtracer.dir/src/core/optics/transmitter.cpp.o"
+  "/home/konstantin/mycode/pathtracer/src/custom/geometry/reconstruction.cpp" "/home/konstantin/mycode/pathtracer/build/CMakeFiles/pathtracer.dir/src/custom/geometry/reconstruction.cpp.o"
+  "/home/konstantin/mycode/pathtracer/src/custom/geometry/sphere.cpp" "/home/konstantin/mycode/pathtracer/build/CMakeFiles/pathtracer.dir/src/custom/geometry/sphere.cpp.o"
+  "/home/konstantin/mycode/pathtracer/src/custom/render/ball.cpp" "/home/konstantin/mycode/pathtracer/build/CMakeFiles/pathtracer.dir/src/custom/render/ball.cpp.o"
+  "/home/konstantin/mycode/pathtracer/src/custom/render/depthpathtracer.cpp" "/home/konstantin/mycode/pathtracer/build/CMakeFiles/pathtracer.dir/src/custom/render/depthpathtracer.cpp.o"
+  "/home/konstantin/mycode/pathtracer/src/custom/render/fixedtooriginplaneviewport.cpp" "/home/konstantin/mycode/pathtracer/build/CMakeFiles/pathtracer.dir/src/custom/render/fixedtooriginplaneviewport.cpp.o"
+  "/home/konstantin/mycode/pathtracer/src/custom/render/importancesampler.cpp" "/home/konstantin/mycode/pathtracer/build/CMakeFiles/pathtracer.dir/src/custom/render/importancesampler.cpp.o"
+  "/home/konstantin/mycode/pathtracer/src/custom/render/previewpathtracer.cpp" "/home/konstantin/mycode/pathtracer/build/CMakeFiles/pathtracer.dir/src/custom/render/previewpathtracer.cpp.o"
+  "/home/konstantin/mycode/pathtracer/src/custom/render/purerandomsampler.cpp" "/home/konstantin/mycode/pathtracer/build/CMakeFiles/pathtracer.dir/src/custom/render/purerandomsampler.cpp.o"
+  "/home/konstantin/mycode/pathtracer/src/custom/render/reflectionpathtracer.cpp" "/home/konstantin/mycode/pathtracer/build/CMakeFiles/pathtracer.dir/src/custom/render/reflectionpathtracer.cpp.o"
+  "/home/konstantin/mycode/pathtracer/src/custom/render/simplerenderer.cpp" "/home/konstantin/mycode/pathtracer/build/CMakeFiles/pathtracer.dir/src/custom/render/simplerenderer.cpp.o"
+  "/home/konstantin/mycode/pathtracer/src/custom/render/simplescene.cpp" "/home/konstantin/mycode/pathtracer/build/CMakeFiles/pathtracer.dir/src/custom/render/simplescene.cpp.o"
+  "/home/konstantin/mycode/pathtracer/src/custom/render/singleadditivetile.cpp" "/home/konstantin/mycode/pathtracer/build/CMakeFiles/pathtracer.dir/src/custom/render/singleadditivetile.cpp.o"
+  "/home/konstantin/mycode/pathtracer/src/custom/render/singlesimpletile.cpp" "/home/konstantin/mycode/pathtracer/build/CMakeFiles/pathtracer.dir/src/custom/render/singlesimpletile.cpp.o"
+  "/home/konstantin/mycode/pathtracer/src/custom/render/topngsaver.cpp" "/home/konstantin/mycode/pathtracer/build/CMakeFiles/pathtracer.dir/src/custom/render/topngsaver.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/freetype2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")

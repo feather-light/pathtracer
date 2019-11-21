3228fe7cc5daa509eb28c279fcff1ddbce0ea91f
feather-light <58053645+feather-light@users.noreply.github.com>
2019-11-22 01:27:25 +0200

    Initial upload

---
+++ build/debug/CMakeFiles/pathtracer.dir/cmake_clean.cmake
@@ -0,0 +1,34 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pathtracer.dir/main.cpp.o"
+  "CMakeFiles/pathtracer.dir/src/core/geometry/point.cpp.o"
+  "CMakeFiles/pathtracer.dir/src/core/geometry/ray.cpp.o"
+  "CMakeFiles/pathtracer.dir/src/core/geometry/tuple.cpp.o"
+  "CMakeFiles/pathtracer.dir/src/core/geometry/vector.cpp.o"
+  "CMakeFiles/pathtracer.dir/src/core/optics/emitter.cpp.o"
+  "CMakeFiles/pathtracer.dir/src/core/optics/laws.cpp.o"
+  "CMakeFiles/pathtracer.dir/src/core/optics/material.cpp.o"
+  "CMakeFiles/pathtracer.dir/src/core/optics/redistributor.cpp.o"
+  "CMakeFiles/pathtracer.dir/src/core/optics/reflector.cpp.o"
+  "CMakeFiles/pathtracer.dir/src/core/optics/spectrum.cpp.o"
+  "CMakeFiles/pathtracer.dir/src/core/optics/transmitter.cpp.o"
+  "CMakeFiles/pathtracer.dir/src/custom/geometry/reconstruction.cpp.o"
+  "CMakeFiles/pathtracer.dir/src/custom/geometry/sphere.cpp.o"
+  "CMakeFiles/pathtracer.dir/src/custom/render/ball.cpp.o"
+  "CMakeFiles/pathtracer.dir/src/custom/render/depthpathtracer.cpp.o"
+  "CMakeFiles/pathtracer.dir/src/custom/render/fixedtooriginplaneviewport.cpp.o"
+  "CMakeFiles/pathtracer.dir/src/custom/render/previewpathtracer.cpp.o"
+  "CMakeFiles/pathtracer.dir/src/custom/render/progressrenderer.cpp.o"
+  "CMakeFiles/pathtracer.dir/src/custom/render/purerandomsampler.cpp.o"
+  "CMakeFiles/pathtracer.dir/src/custom/render/reflectionpathtracer.cpp.o"
+  "CMakeFiles/pathtracer.dir/src/custom/render/simplerenderer.cpp.o"
+  "CMakeFiles/pathtracer.dir/src/custom/render/simplescene.cpp.o"
+  "CMakeFiles/pathtracer.dir/src/custom/render/singlesimpletile.cpp.o"
+  "CMakeFiles/pathtracer.dir/src/custom/render/topngsaver.cpp.o"
+  "pathtracer"
+  "pathtracer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pathtracer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
